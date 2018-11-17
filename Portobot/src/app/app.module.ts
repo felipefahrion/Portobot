2dca0e248485dee5b9bf8f81bf45600482c60dbe
felipefahrion <felipe.fahrion@gmail.com>
2018-11-17 16:04:00 -0200

    adding some buttons

--- Portobot/src/app/app.module.ts
+++ Portobot/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { GetStartedComponent } from './get-started/get-started.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GetStartedComponent
   ],
   imports: [
     BrowserModule,

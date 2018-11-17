c6f786517fced062839e4b02092503e3e18e7dc0
felipefahrion <felipe.fahrion@gmail.com>
2018-11-17 17:03:17 -0200

    adding routing

--- Portobot/src/app/app-routing.module.ts
+++ Portobot/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { ChatViewComponent } from './chat-view/chat-view.component';
+
+const routes: Routes = [
+  { path: 'chat', component: ChatViewComponent }
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ],
   declarations: []
 })
 export class AppRoutingModule { }

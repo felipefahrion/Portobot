2c377281f066ea4d86bff7d651089d7764754e04
felipefahrion <felipe.fahrion@gmail.com>
2018-11-18 07:10:31 -0200

    lalalalla

--- Portobot/src/app/app.module.ts
+++ Portobot/src/app/app.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { GetStartedComponent } from './get-started/get-started.component';
 import { ChatViewComponent } from './chat-view/chat-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GetStartedComponent,
     ChatViewComponent,
   ],
   imports: [
